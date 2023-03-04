736c39d7f3fa6121354af8684cef270595622fe7
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-04 17:54:52 -0600

    Continually get input if not number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,27 +10,9 @@ fn main() {
     let secret_number = rand::thread_rng()
         .gen_range(1..11);
 
-    let mut guess = String::new();
+    let guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
-
-    // Now we need to cast the string to an integer
-    // We can do this with the parse method
-    let guess: u32 = match guess
-        .trim()
-        .parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                // In the case that there was an error,
-                // set the guess to 0
-                0
-            },
-        };
+    let guess = get_guess(guess);
 
     let did_win: bool;
 
@@ -56,4 +38,27 @@ fn main() {
     } else {
         println!("You won! Yay!")
     }
+}
+
+fn get_guess(mut guess: String) -> u32 {
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    // Now we need to cast the string to an integer
+    // We can do this with the parse method
+    let guess: u32 = match guess
+        .trim()
+        .parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                // Clear the guess
+                guess = String::new();
+                get_guess(guess)
+            },
+        };
+    guess
 }
