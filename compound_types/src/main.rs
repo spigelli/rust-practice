7b1870f17a7154bfeba46d7a5fd9d0575d9578c2
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-17 15:54:41 -0500

    Added example: Compound Types

--- compound_types/src/main.rs
+++ compound_types/src/main.rs
@@ -4,10 +4,31 @@ fn tuples() {
 
     // Assign by matching
     let (x, y, z) = tup;
-    println!("The value of y is: {}", y);
+    println!("The value of x is {}", x);
+    println!("The value of y is {}", y);
+    println!("The value of z is {}", z);
+
+    println!("The value of tup.0 is {}", tup.0);
 }
+
+fn arrays() {
+    // Arrays must be of the same type
+    // Arrays are stack allocated
+    // Arrays should be fixed length
+    
+    let a = [1, 2, 3, 4, 5];
+
+    // Arrays can have a default value
+    let b = [0; 10]; // 10 zeros
+    for i in 0..10 {
+        println!("b[{}] = {}", i, b[i])
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
     tuples();
+    println!("\n");
+    arrays();
 }
