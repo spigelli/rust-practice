bb550f10064c55029b0fe87a24a4e536f57fb119
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-17 15:44:16 -0500

    Added compound types

--- compound_types/src/main.rs
+++ compound_types/src/main.rs
@@ -1,3 +1,13 @@
+fn tuples() {
+    // Declare a tuple that contains an integer, a floating-point number, and a boolean
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // Assign by matching
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+}
+
 fn main() {
     println!("Hello, world!");
+    tuples();
 }
