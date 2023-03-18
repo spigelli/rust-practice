f13e44aa1165595fc9bc67032b680d0c8a2f3f52
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-17 19:18:37 -0500

    Added example: Modules

---
+++ modules/src/main.rs
@@ -0,0 +1,14 @@
+// This module is in src/garden/mod.rs
+mod garden;
+use crate::garden::vegetables::Asparagus;
+
+fn main() {
+    println!("Hello, world!");
+    let c: Asparagus = String::from("
+      @@
+      @@
+      @@
+      |
+      |
+    ");
+}
